741e1c12fcab23bd4d7c3a487163bbef2358ef7e
Lovisavi <lovisa.virgin@gmail.com>
2023-03-16 09:14:27 +0100

    Continued controller practise

--- ControllerPractise/Controllers/HelloWorldController.cs
+++ ControllerPractise/Controllers/HelloWorldController.cs
@@ -5,13 +5,10 @@ namespace MvcMovie.Controllers;
 
 public class HelloWorldController : Controller
 {
-    // 
-    // GET: /HelloWorld/
     public IActionResult Index()
     {
         return View();
     }
-
     public IActionResult Welcome(string name, int numTimes = 1)
     {
         ViewData["Message"] = "Hello " + name;
